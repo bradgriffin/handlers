e95f9d668e37e304890fa722b22956fc5430f5c2
Brad Griffin <bgriffin20@gmail.com>
2012-08-01 20:39:28 -0500

    Add a ruby template handler

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class NavigationTest < ActiveSupport::IntegrationCase
-  test "truth" do
-    assert_kind_of Dummy::Application, Rails.application
+  test '.rb template handler' do 
+  	visit '/handlers/index'
+  	expected = 'This is my first <b>template handler</b>!'
+  	assert_match expected, page.body
   end
 end
