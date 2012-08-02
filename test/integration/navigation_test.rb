205ddd1307049636a3e62473fc01dea49d7da9ad
Brad Griffin <bgriffin20@gmail.com>
2012-08-01 21:12:42 -0500

    create .string template handler

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class NavigationTest < ActiveSupport::IntegrationCase
+  test '.string template handler' do
+    visit '/handlers/show'
+    expected = 'Congratulations! You just created another template handler!'
+    assert_match expected, page.body
+  end
+
   test '.rb template handler' do 
   	visit '/handlers/index'
   	expected = 'This is my first <b>template handler</b>!'
